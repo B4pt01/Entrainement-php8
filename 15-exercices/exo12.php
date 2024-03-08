1e8bfcf97ba85f3e0c7edb31c9b9a02a14c4a388
baptiste vauthier <ba.vauthier@gmail.com>
2024-03-08 15:20:56 +0100

    exo 12

--- 15-exercices/exo12.php
+++ 15-exercices/exo12.php
@@ -2,7 +2,32 @@
 include("partials/header.php");
 include("partials/menu.php");
 ?>
-
+<form action="#" method="post">
+    <label for="Montantcmd">Montant de votre commande : </label>
+    <input type="text" name="Montantcmd" id="Montantcmd">
+    <select name="typecmd">
+        <option value="standard">standard</option>
+        <option value="express">express</option>
+        <option value="internationnal">internationnal</option>
+    </select>
+    <button type="submit">valider</button>
+</form>
+<?php
+if (isset($_POST['Montantcmd']) && isset($_POST['typecmd'])) {
+    $Montantcmd = floatval($_POST['Montantcmd']);
+    $fps = $Montantcmd > 50 ? 0 : 5;
+    $fpe = $Montantcmd > 100 ? 5 : 10;
+    $fpi = $Montantcmd < 50 ? 25 : 15;
+    switch ($_POST['typecmd']) {
+        case 'standard':
+            echo "prix total : " . ($Montantcmd + $fps);
+        case 'express':
+            echo "prix total : " . ($Montantcmd + $fpe);
+        case 'internationnal':
+            echo "prix total : " . ($Montantcmd + $fpi);
+    }
+}
+?>
 <?php
 include("partials/footer.php");
 ?>
