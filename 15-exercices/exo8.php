8c60b4e8684f0d624162d731ce7c50b60770a159
baptiste vauthier <ba.vauthier@gmail.com>
2024-03-08 14:24:16 +0100

    exo 8

--- 15-exercices/exo8.php
+++ 15-exercices/exo8.php
@@ -2,7 +2,29 @@
 include("partials/header.php");
 include("partials/menu.php");
 ?>
+<?php
+$tableau = array(2, 6, 12, 5, 26, 34, 40, 60);
 
+function tableau_paire($tableau)
+{
+    $paire = true;
+    foreach ($tableau as $valeur) {
+        if ($valeur % 2 != 0) {
+            $paire = false;
+            break;
+        }
+    }
+    return $paire;
+}
+
+if (tableau_paire($tableau)) {
+    echo "Le tableau ne contient que des valeurs paires.";
+} else {
+    echo "Le tableau contient au moins une valeur impaire.";
+}
+
+
+?>
 <?php
 include("partials/footer.php");
 ?>
